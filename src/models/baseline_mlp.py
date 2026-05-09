c67884095533780d128d6b7244c4ee4525a7d0a5
Muhammad Hanzla <144888680+MuhammadHanzla17@users.noreply.github.com>
2026-05-10 01:56:52 +0500

    Add files via upload

---
+++ src/models/baseline_mlp.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bda0557b-8c7b-428f-9be7-a31f6b055b83",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
